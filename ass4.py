d6572f1389cf78a96087e063c8015ef8e94c6cde
tamiryaffe <tamiryaffe144@gmail.com>
2020-07-15 19:45:02 +0300

    commits

---
+++ ass4.py
@@ -0,0 +1,16 @@
+import time
+
+
+def part1():
+    pass
+
+
+def main():
+    part1()
+    pass
+
+
+if __name__ == '__main__':
+    start_time = time.time()
+    main()
+    print("--- %s seconds ---" % (time.time() - start_time))
